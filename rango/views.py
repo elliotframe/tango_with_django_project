3825afbab66f76fcea52d14e85d2d23694c4fe54
Elliot Frame <2732558f@student.gla.ac.uk>
2024-01-25 15:18:18 +0000

    Links to about and index

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Happening son")
+    return HttpResponse("Happening son \n <a href='/rango/about/'>Click here</a> to go back to the index page")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page \n <a href='/rango/'>link text</a>")
